30106800a63ec7f45b47663e536a0f22891dbaa8
HenriPKemppainen <HenriPKemppainen@outlook.com>
2021-02-11 19:34:18 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+  const [book, setBook] = useState({
+    title: '',
+    pages: '',
+    publishDate: '',
+  })
+
+  const onChange = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const onBookSubmit = (e) => {
+    e.preventDefault()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Book Library</h1>
+
+      <form onSubmit={onBookSubmit}>
+        <div>
+          <label htmlFor="book-title">Title:</label>
+          <input
+            type="text"
+            value={book.title}
+            onChange={onChange}
+            name="title"
+            id="book_title"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="book-pages">Number of pages:</label>
+          <input
+            type="number"
+            value={book.pages}
+            onChange={onChange}
+            name="pages"
+            id="book_pages"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="book-publish-date">Date of publishing:</label>
+          <input
+            type="date"
+            value={book.publishingDate}
+            onChange={onChange}
+            name="publishDate"
+            id="book_publishing_date"
+          />
+        </div>
+
+        <div>
+          <button type="submit">Save</button>
+        </div>
+      </form>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
